07cc8e4bd6259dc35c9b58f48adad7a271fff225
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-12-04 11:49:32 +0200

    add fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 
+$(document).ready(function () {
+    $('.more-btn').click(function () {
+        var $this = $(this);
+        var $infoWrap = $this.siblings('.content-wrapper');
+        var currentHeight = $infoWrap.outerHeight();
+        var newHeight = (currentHeight === 360) ? $infoWrap.get(0).scrollHeight : 360;
+
+        $infoWrap.animate({
+            height: newHeight
+        }, 700);
+
+        
+        $this.toggleClass('active');
+    });
+});
+
+
+
+
+
+
 AOS.init({
     duration: 1000,
     disable: 'mobile',
